d4d8fc18ab204698f55091cde23e5e0b6e1ca28e
palakpari patel <palakparipatel@gmail.com>
2021-09-02 22:27:46 -0400

    changes to hey

--- src/test/java/com/yoshallc/store/YoshLLC.java
+++ src/test/java/com/yoshallc/store/YoshLLC.java
@@ -3,6 +3,6 @@ package com.yoshallc.store;
 public class YoshLLC {
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hey world");
     }
 }
